9c2dbc66c128776bc58c67c66da23dc80c3029d8
Alimul Hassan <alhassan069@gmail.com>
2021-12-24 00:06:55 +0530

    made the static page using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+var OS = ["Android", "Blackberry", "iPhone", "Windows Phone"];
+var Man = ["Samsung", "HTC", "Micromax", "Apple"];
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mobile Operating System</h1>
+      <ul>{OS.map((e) => (
+        <li>{e}</li>
+      ))}
+      </ul>
+      <br />
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+        {Man.map((e) => (
+          <Mbl nam={e} /> 
+        ))}
+      </ul>
     </div>
   );
+}
+
+function Mbl({nam}){
+  
+  return <li>{nam}</li>
+  
 }
 
 export default App;
